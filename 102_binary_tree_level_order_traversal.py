c80d771ac322351374fb37ff805ad8a5a2d5efa0
Ely Ding <yiding1@uchicago.edu>
2019-07-07 16:50:35 +0800

    1st commit

--- 102_binary_tree_level_order_traversal.py
+++ 102_binary_tree_level_order_traversal.py
@@ -28,3 +28,38 @@ def levelOrder(self, root):
         LRpair = [(node.left, node.right) for node in level]
         level = [leaf for LR in LRpair for leaf in LR if leaf]
     return ans'''
+
+# BFS:    
+from collections import deque
+class Solution:
+    """
+    @param root: A Tree
+    @return: Level order a list of lists of integer
+    """
+    def levelOrder(self, root):
+        if not root:
+            return []
+        
+        def children(node):
+            result=[]
+            if node.left:
+                result.append(node.left)
+            if node.right:
+                result.append(node.right)
+            return result
+        
+        queue,visited,result = deque(),set(),[[root.val]]
+        queue.append(root)
+        visited.add(root)
+        while len(queue):
+            size,tmp = len(queue),[]
+            for _ in range(size):
+                head = queue.popleft()
+                for child in children(head):
+                    if child not in visited:
+                        visited.add(child)
+                        queue.append(child)
+                        tmp.append(child.val)
+            if tmp:
+                result.append(tmp)
+        return result
--- 200_number_of_islands.py
+++ 200_number_of_islands.py
@@ -28,3 +28,13 @@ def numIslands(self, grid: List[List[str]]):
                         visited.add(neighbor)
                         queue.append(neighbor)
     return island_count
+
+#this guy is smart: BFS in place 
+def numIslands(self, grid):
+    def sink(i, j):
+        if 0 <= i < len(grid) and 0 <= j < len(grid[i]) and grid[i][j] == '1':
+            grid[i][j] = '0'
+            map(sink, (i+1, i-1, i, i), (j, j, j+1, j-1))
+            return 1
+        return 0
+    return sum(sink(i, j) for i in range(len(grid)) for j in range(len(grid[i])))
